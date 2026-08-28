69e27a8d43566fcca01c364b10be9eefcc2fd0ba
Andrew-742001 <andrew742001@gmail.com>
2026-08-27 23:58:11 -0700

    Update program comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * This is a simple Hello World program in C++.
  */
 int main() {
-    // Print Hello World to the terminal
+    // Display Hello World in the terminal!
     cout << "Hello World!" << endl;
 
     return 0;
